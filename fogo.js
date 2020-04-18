3bbcf858ef74bd9722fae7aad0872295de587d37
joaowilian <joaowilian@yahoo.com.br>
2020-04-18 15:26:24 -0300

    Barra Slider

--- fogo.js
+++ fogo.js
@@ -1,23 +1,49 @@
 const fogoPixelArray =[]  //array linear, todos os pixel do fogo vão estar um do lado do outro  
-const larguraDoFogo = 40
-const alturaDoFogo = 60
+const larguraDoFogo = 50
+const alturaDoFogo = 50
 const PaletaDeCores = [{"r":7,"g":7,"b":7},{"r":31,"g":7,"b":7},{"r":47,"g":15,"b":7},{"r":71,"g":15,"b":7},{"r":87,"g":23,"b":7},{"r":103,"g":31,"b":7},{"r":119,"g":31,"b":7},{"r":143,"g":39,"b":7},{"r":159,"g":47,"b":7},{"r":175,"g":63,"b":7},{"r":191,"g":71,"b":7},{"r":199,"g":71,"b":7},{"r":223,"g":79,"b":7},{"r":223,"g":87,"b":7},{"r":223,"g":87,"b":7},{"r":215,"g":95,"b":7},{"r":215,"g":95,"b":7},{"r":215,"g":103,"b":15},{"r":207,"g":111,"b":15},{"r":207,"g":119,"b":15},{"r":207,"g":127,"b":15},{"r":207,"g":135,"b":23},{"r":199,"g":135,"b":23},{"r":199,"g":143,"b":23},{"r":199,"g":151,"b":31},{"r":191,"g":159,"b":31},{"r":191,"g":159,"b":31},{"r":191,"g":167,"b":39},{"r":191,"g":167,"b":39},{"r":191,"g":175,"b":47},{"r":183,"g":175,"b":47},{"r":183,"g":183,"b":47},{"r":183,"g":183,"b":55},{"r":207,"g":207,"b":111},{"r":223,"g":223,"b":159},{"r":239,"g":239,"b":199},{"r":255,"g":255,"b":255}]
+var alteraAltura = 36
+
 
 //https://www.youtube.com/watch?v=fxm8cadCqbs
+function barra(){    
+    var slider = document.getElementById("rangeBarra");
+        
+    var output = document.getElementById("demo");
+    output.innerHTML = slider.value;
+
+    slider.oninput = function() {
+        output.innerHTML = this.value;
+
+       alteraAltura = output.innerHTML
+    
+        console.log(alteraAltura,'alterar autura')
+        
+
+    }
+    criandoOrigemDoFogo()
+
+    
+}
+
 
 
 function inicial(){
     calculaEstruturaDados()     
     criandoOrigemDoFogo()
-    renderizaFogo()
+    renderizaFogo()   
+  
 
-    setInterval(calculaPropagacaoFogo,50)
+    setInterval(calculaPropagacaoFogo,50) // loop infinito
+
+    
 
 }
 
 function calculaEstruturaDados(){
     const numeroDePixel = larguraDoFogo * alturaDoFogo //multiplicando a largura e altura encontra todos os pixel do fogo
-    console.log(numeroDePixel,'quantiadade de pixel criada')
+    //console.log(numeroDePixel,'quantiadade de pixel criada')
+   
     for (let i = 0 ; i < numeroDePixel ; i++ ){
         fogoPixelArray[i] = 0 //incrementa 0 em todos os indices do array, pq a intencidade do fogo e 0 
     }
@@ -33,6 +59,8 @@ function atualizaIntencidadeDoFogo(pixelAtual){
     //calcular o enfraquecimento da intencidade do fogo
     const decair = Math.floor(Math.random() * 3) // gera fogo numeros aleatorios    
     //const decair = 1 // gera fogo com todas as colunas iguais
+
+    
    
     const intencidadePixeDeBaixo =  fogoPixelArray[indeceDoPixelAbaixo]
     const novaIntencidadeDoFogo = 
@@ -43,6 +71,7 @@ function atualizaIntencidadeDoFogo(pixelAtual){
     // para fazer como se tivesse vento no fogo o pixel atualiza o pixel ao lado 
     fogoPixelArray[pixelAtual - decair] = novaIntencidadeDoFogo // fogo para lado esquerdo
     //fogoPixelArray[pixelAtual + decair] = novaIntencidadeDoFogo // fogo para lado direito
+    
 }
 
 function calculaPropagacaoFogo(){
@@ -51,6 +80,7 @@ function calculaPropagacaoFogo(){
             const pixelindice = coluna + ( larguraDoFogo * linha )
 
             atualizaIntencidadeDoFogo(pixelindice)
+            
             
         }
     }
@@ -71,10 +101,11 @@ function renderizaFogo(){
 */
     let html = '<table cellpadding=0 cellspacing=0>'
     const debug = false // true -> mostra as cores, false -> numero da tabela de cores
-    for (let linha = 0; linha < alturaDoFogo; linha++){
+    for (let linha = 0; linha < alturaDoFogo ; linha++){
         html += '<tr>' // cria linha        
         for (let coluna = 0; coluna < larguraDoFogo; coluna++){
             const pixelIndice = coluna + (larguraDoFogo * linha)       
+            
             // coluna --> descobre a posicao vertical
             // (larguraDoFogo * linha) --> Descobre a posição horizontal     
 
@@ -103,16 +134,19 @@ function renderizaFogo(){
 }
 
 function criandoOrigemDoFogo(){
+    
     for (let coluna = 0; coluna <= larguraDoFogo; coluna++){
-        console.log(coluna)
+        //console.log(coluna)
         const mudarIntensidade = larguraDoFogo * alturaDoFogo
-        const pixelIndice = (mudarIntensidade - larguraDoFogo) + coluna
-
-        fogoPixelArray[pixelIndice] = 36 // a tabela de cores utilizada no fogo tem 36 cores
+        const pixelIndice = (mudarIntensidade - larguraDoFogo ) + coluna
+       
+        fogoPixelArray[pixelIndice] = alteraAltura // a tabela de cores utilizada no fogo tem 36 cores
+        
     }
+
 /*
  Canto superio direito e o indece do ARRAY
- numero no centro e a intencidade do fogo
+ numero no centro e a intensidade do fogo
  _______ _______ _______       _______
 |      0|      1|      2|     |      9|
 |   0   |   0   |   0   | ... |   0   | 
@@ -129,15 +163,6 @@ function criandoOrigemDoFogo(){
 |_______|_______|_______|     |_______|       
 
 */
-
-
-
-
-
-
-
-
-
 
 
 
